6e55f00951ac0ec1353c45dc55344f10101b97d7
Sahil Patrekar <sahilpatrekar@192.168.1.3>
2026-01-18 22:12:52 +0530

    Loop file

---
+++ loop.py
@@ -0,0 +1,15 @@
+# Using looping constructs in Python
+# For loop example
+for i in range (1,10,1):
+    print(i)
+
+# While loop example
+a = 0
+while a <= 10:
+    print(a)
+    a += 1
+
+#Nested loop example
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(f"i: {i}, j: {j}")        
